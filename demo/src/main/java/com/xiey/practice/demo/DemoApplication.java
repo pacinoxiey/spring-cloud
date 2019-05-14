d8527169332e82ae445ef07d3d972e0afa021b64
xiey <1004056738@qq.com>
2019-05-14 14:10:07 +0800

    'configserver'

--- demo/src/main/java/com/xiey/practice/demo/DemoApplication.java
+++ demo/src/main/java/com/xiey/practice/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.xiey.practice.demo.api")
+
 public class DemoApplication {
 
     public static void main(String[] args) {
