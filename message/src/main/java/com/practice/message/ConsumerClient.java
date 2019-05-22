97c9e0903f4284939002ed9f14c99580fa1cf5ae
xiey <1004056738@qq.com>
2019-05-22 14:51:00 +0800

    '原生方式实现mq生产与消费'

--- message/src/main/java/com/practice/message/ConsumerClient.java
+++ message/src/main/java/com/practice/message/ConsumerClient.java
@@ -1,5 +1,9 @@
 package com.practice.message;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * @author xiey
  * @date 2019/5/20 16:18
@@ -7,6 +11,25 @@ package com.practice.message;
  */
 public class ConsumerClient {
 
+
+    public static void main(String[] args) throws JMSException {
+
+        ConnectionFactory factory = new ActiveMQConnectionFactory(
+                "admin", "admin", "tcp://127.0.0.1:61616");
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = session.createTopic("test-topic");
+        MessageConsumer consumer = session.createConsumer(topic);
+
+
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                System.err.println("receive message:" + message);
+            }
+        });
+    }
 
 
 }
